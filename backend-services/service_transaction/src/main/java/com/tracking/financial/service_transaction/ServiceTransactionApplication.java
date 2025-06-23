7a9c6ae25e4620f8aa4e1c0b48b3534b909a0730
Fernandosiahaaan <siahaanfernando708@gmail.com>
2025-06-23 20:19:39 +0700

    init project

---
+++ backend-services/service_transaction/src/main/java/com/tracking/financial/service_transaction/ServiceTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.tracking.financial.service_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTransactionApplication.class, args);
+	}
+
+}
